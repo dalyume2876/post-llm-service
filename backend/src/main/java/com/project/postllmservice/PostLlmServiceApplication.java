f6c730a1822bfc6bc0e61b7d5dff6bdfa0148565
dalyume2876 <bold20030@gmail.com>
2026-09-22 09:47:19 +0900

    first commit

---
+++ backend/src/main/java/com/project/postllmservice/PostLlmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.postllmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostLlmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostLlmServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/postllmservice/PostLlmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.postllmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostLlmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
